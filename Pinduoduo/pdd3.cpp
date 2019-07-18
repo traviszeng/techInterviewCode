7a1342c35fbb828be7d4abb18fed33d230531603
traviszeng <traviszeng7@gmail.com>
2019-07-18 20:59:25 +0800

    完成拼多多

--- Pinduoduo/pdd3.cpp
+++ Pinduoduo/pdd3.cpp
@@ -23,4 +23,118 @@ A ¹úµÄÊÖ»úºÅÂëÓÉÇÒ½öÓÉ N Î»Ê®½øÖÆÊı×Ö(0-9)×é³É¡£Ò»¸öÊÖ»úºÅÂëÖĞÓĞÖÁÉÙ K Î»Êı×ÖÏàÍ
 ËµÃ÷
 »¨·ÑÎª4µÄ·½°¸ÓĞÁ½ÖÖ£º777577Óë777775£¬Ç°Õß×ÖµäĞò¸üĞ¡¡£
 */
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<map>
+using namespace std;
+
+bool cmp(const pair<char,int>& a,const pair<char,int>& b){
+	return a.second < b.second;
+}
+
+
+int find(map<char, int> nmap,char key,int K,string& result) {
+	int needNum = K - nmap[key];
+	int k = 1;
+	int sum = 0;
+	while (needNum>0) { 
+		map<char, int>::iterator temp = nmap.find(key + k);
+		if (temp != nmap.end()) {
+			//ÏÈÑ¡Ğ¡Î»ÖÃµÄ
+			if (nmap[key + k] <= needNum) {
+				for (int i = 0; i < result.length(); i++)
+					if (result[i] == key + k)
+						result[i] = key;
+				needNum -= nmap[key + k];
+				sum += k * nmap[key + k];
+			}
+			else {
+				sum += k * needNum;
+				for (int i = 0, num = 0; i<result.length() && num<needNum; i++) {
+					if (result[i] == key + k) {
+						result[i] = key;
+						num++;
+					}
+				}
+				
+
+				needNum = 0;
+				break;
+				return sum;
+			}
+		}
+
+
+		temp = nmap.find(key - k);
+		if (temp != nmap.end()) {
+			//ÏÈÑ¡Ğ¡Î»ÖÃµÄ
+			if (nmap[key - k] <= needNum) { //È«²¿Ìæ»»
+				for (int i = 0; i < result.length(); i++)
+					if (result[i] == key - k)
+						result[i] = key;
+				needNum -= nmap[key - k];
+				sum += k * nmap[key - k];
+			}
+			else {
+				
+				for (int i = result.length() - 1, num = 0; i >= 0 && num<needNum; i--) {
+					if (result[i] == key - k) {
+						result[i] = key;
+						num++;
+					}
+				}
+				sum += k * needNum;
+				needNum = 0;
+				break;
+				return sum;
+			}
+		}
+		
+		k++;
+
+		
+	}
+	//cout << result;
+	return sum;
+}
+
+int main() {
+	int N, K;
+	string number;
+	cin >> N >> K;
+	cin >> number;
+	map<char, int> numMap;
+	for (int i = 0; i < N; i++) {
+		if (numMap.find(number[i]) == numMap.end()) {
+			numMap[number[i]] = 1;
+		}
+		else {
+			numMap[number[i]] += 1;
+		}
+	}
+	/*map<char, int>::iterator iter = max_element(numMap.begin(), numMap.end(), cmp);
+	if (iter!=numMap.end() && iter->second>=K) {
+		cout << 0 << endl << number;
+		return 0;
+	}*/
+	int min = N * 9;
+	char key;
+	string finalResult = "";
+	
+	map<char, int>::iterator start = numMap.begin();
+	while (start != numMap.end()) {
+		string result = number;
+		int temp = find(numMap, start->first,K,result);
+		if (temp < min) {
+			min = temp;
+			key = start->first;
+			finalResult = result;
+		}
+		start++;
+	}
+
+	cout << min<<endl<<finalResult;
+	system("pause");
+}
 
